afe362c5b84b054b4b911d1c2a95a26e16e35262
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2024-10-17 15:18:55 +0530

    COMMIT

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,18 +25,16 @@ export default function Header({}) {
 
   return (
     <>
-      <CartModal
-        ref={modal}
-        title="Your Cart"
-        actions={modalActions}
-      />
+      <CartModal ref={modal} title="Your Cart" actions={modalActions} />
       <header id="main-header">
         <div id="main-title">
           <img src="logo.png" alt="Elegant model" />
           <h1>Elegant Context</h1>
         </div>
         <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+          <button className="cart-items-fx" onClick={handleOpenCartClick}>
+            Cart ({cartQuantity})
+          </button>
         </p>
       </header>
     </>
